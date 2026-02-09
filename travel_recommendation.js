15f00a4fd0baa61a351013bed83290294614e1e6
luisplis <luisplis@gmail.com>
2026-02-09 03:16:47 +0100

    redirect and end all!

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -14,6 +14,7 @@ function apiFinder() {
       if (finder) {
         let content = '';
         finder.cities.forEach(item => {
+          const datetimes = dateTimes(item.name);
           content+=`
           <section class="card col-12 col-md-4 col-lg-3">
             <article class="card-body">
@@ -22,6 +23,7 @@ function apiFinder() {
               <img src="img/${item.imageUrl}" alt="${item.name}" width="250" height="150"/>
               </div>
               <p class="my-3">${item.description}</p>
+              <p class="my-2"><small>Time Zone: <code>${datetimes}</code></small></p>
             </article>
           </section>`
         });
@@ -42,3 +44,32 @@ function clsForm() {
   document.getElementById("term").value = "";
 }
 document.getElementById('clear').addEventListener('click', clsForm);
+
+function dateTimes(zone) {
+  let datetime = '';
+  const timeZoneMap = {
+    "Sydney, Australia": "Australia/Sydney",
+    "Melbourne, Australia": "Australia/Melbourne",
+    "Tokyo, Japan": "Asia/Tokyo",
+    "Kyoto, Japan": "Asia/Tokyo",
+    "Rio de Janeiro, Brazil": "America/Sao_Paulo",
+    "São Paulo, Brazil": "America/Sao_Paulo",
+    "Angkor Wat, Cambodia": "Asia/Phnom_Penh",
+    "Taj Mahal, India": "Asia/Kolkata",
+    "Bora Bora, French Polynesia": "Pacific/Tahiti",
+    "Copacabana Beach, Brazil": "America/Sao_Paulo"
+  };
+  try {
+    //const locate = zone.split(',').reverse().join('/').trim().replace(/\s+/g, '_');
+    const locate = Object.keys(timeZoneMap).find(key => 
+      key.toLowerCase().includes(input)
+    );
+    const options = { timeZone: locate, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    datetime = new Date().toLocaleTimeString('en-US', options); 
+    console.log("Current time in "+zone+" is ready!");
+  } catch (error) {
+    datetime = new Date().toLocaleTimeString();
+    console.log("Current time in "+zone+" error!");
+  }
+  return datetime;
+}
